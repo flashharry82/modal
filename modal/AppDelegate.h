b5cc0e7977e2ca12c244314af50c8c0e431f1739
Alan Stirling <alan@johnstonnorth.com>
2012-06-20 11:56:18 +0100

    Initial Commit

---
+++ modal/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  modal
+//
+//  Created by Alan Stirling on 20/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ modal/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  modal
+//
+//  Created by Alan Stirling on 20/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
